74437b6cdcee863dfa8f58b60d0f53e4f7a577ad
salhyun <salhyun@naver.com>
2017-10-19 19:07:30 +0900

    control ar camera

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class UIManager : MonoBehaviour{
+
+	public int mClickCount = 0;
+	public GameObject duck;
+	public GameObject capsule;
+	public Camera arCamera;
+
+	public void onClickButton()
+	{
+		//Debug.Log("duck pos(" + duck.transform.position.x + ", " + duck.transform.position.y + ", " + duck.transform.position.z + ")");
+
+		//duck.transform.Translate (30.0f, 0.0f, 0.0f);
+		//duck.transform.position.Set (duck.transform.position.x+40.0f, duck.transform.position.y, duck.transform.position.z);
+		//capsule.transform.position.Set (duck.transform.position.x+40.0f, duck.transform.position.y, duck.transform.position.z);
+		//capsule.transform.position.x = capsule.transform.position.x + 50.0f;
+
+		Vector3 capsulePos = capsule.transform.position;
+		Debug.Log("capsule pos(" + capsulePos.x + ", " + capsulePos.y + ", " + capsulePos.z + ")");
+
+		capsule.transform.Translate (1.0f, 0.0f, 0.0f);
+
+		GameObject copyDuck = Instantiate(duck, capsule.transform.position, capsule.transform.rotation) as GameObject;
+		Debug.Log ("copyDuck = " + copyDuck.name);
+		Debug.Log ("parent = " + copyDuck.transform.parent.name);
+
+		Vector3 lookat = arCamera.transform.forward;
+		Debug.Log("CameraLookat (" + lookat.x + ", " + lookat.y + ", " + lookat.z + ")");
+
+		Debug.Log ("onClickButton count = " + mClickCount);
+		mClickCount++;
+	}
+}
---
+++ Assets/Scripts/duckTrackableEventHandler.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vuforia
+{
+	public class duckTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+	{
+		#region PRIVATE_MEMBER_VARIABLES
+
+		private TrackableBehaviour mTrackableBehaviour;
+
+		#endregion // PRIVATE_MEMBER_VARIABLES
+
+		public GameObject duck;
+		public GameObject capsule;
+		public Camera ARCamera;
+
+		#region UNTIY_MONOBEHAVIOUR_METHODS
+
+		void Start()
+		{
+			mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+			if (mTrackableBehaviour)
+			{
+				mTrackableBehaviour.RegisterTrackableEventHandler(this);
+			}
+		}
+
+		#endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+
+
+		#region PUBLIC_METHODS
+
+		/// <summary>
+		/// Implementation of the ITrackableEventHandler function called when the
+		/// tracking state changes.
+		/// </summary>
+		public void OnTrackableStateChanged(
+			TrackableBehaviour.Status previousStatus,
+			TrackableBehaviour.Status newStatus)
+		{
+			if (newStatus == TrackableBehaviour.Status.DETECTED ||
+				newStatus == TrackableBehaviour.Status.TRACKED ||
+				newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+			{
+				OnTrackingFound();
+			}
+			else
+			{
+				OnTrackingLost();
+			}
+		}
+
+		#endregion // PUBLIC_METHODS
+
+
+
+		#region PRIVATE_METHODS
+
+
+		private void OnTrackingFound()
+		{
+			Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+			Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+			// Enable rendering:
+			foreach (Renderer component in rendererComponents)
+			{
+				component.enabled = true;
+			}
+
+			// Enable colliders:
+			foreach (Collider component in colliderComponents)
+			{
+				component.enabled = true;
+			}
+
+			Debug.Log("duck Trackable " + mTrackableBehaviour.TrackableName + " found");
+
+			Debug.Log ("ImageTargetPos : " + this.transform.position.ToString());
+			duck.transform.position = this.transform.position;
+		}
+
+
+		private void OnTrackingLost()
+		{
+			Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+			Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+
+			// Disable rendering:
+			foreach (Renderer component in rendererComponents)
+			{
+				component.enabled = false;
+			}
+
+			// Disable colliders:
+			foreach (Collider component in colliderComponents)
+			{
+				component.enabled = false;
+			}
+
+			Debug.Log("duck Trackable " + mTrackableBehaviour.TrackableName + " lost");
+
+			Debug.Log ("ARCamera lookat : " + ARCamera.transform.forward.ToString ());
+			Debug.Log ("ARCamera pos : " + ARCamera.transform.position.ToString ());
+			Debug.Log ("capsule pos : " + capsule.transform.position.ToString ());
+
+			Vector3 pos = ARCamera.transform.position + ARCamera.transform.forward.normalized * 50.0f;
+			duck.transform.position = capsule.transform.position;
+		}
+
+		#endregion // PRIVATE_METHODS
+	}
+}
